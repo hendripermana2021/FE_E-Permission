d3230f589161d5b5dbee921ec730d4c6dfa8f159
hendripermana2021 <hendripermana60@gmail.com>
2024-03-28 08:38:21 +0700

    update code

--- src/Server.js
+++ src/Server.js
@@ -1,4 +1,4 @@
-const serverDev = `https://be-permission.up.railway.app/`;
+const serverDev = `https://be-permission.up.railway.app/v1/api/`;
 // const serverProduction = "";
 
 export default serverDev;
