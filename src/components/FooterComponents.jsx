bbbf4a04878512ed22aa571072d03f69f5f3bbaa
hendripermana2021 <hendripermana60@gmail.com>
2024-04-05 14:17:25 +0700

    Upload new code

--- src/components/FooterComponents.jsx
+++ src/components/FooterComponents.jsx
@@ -1,11 +1,12 @@
 function FooterComponent() {
-  const currentYear = new Date().getFullYear();
-
   return (
     <footer className="footer col-lg-12 col-md-12 flex-row">
       <div className="d-flex justify-content-center justify-content-sm-between align-items-center">
         <span className="text-muted text-center d-block d-sm-inline-block">
-          &copy; Hendri_Permana {currentYear}
+          Created By : Hendri Permana Putra
+        </span>
+        <span className="text-muted text-center d-block d-sm-inline-block">
+          &copy; Created2024
         </span>
         <span className="text-muted text-center d-block d-sm-inline-block">
           Powered by BOOTSTRAP
